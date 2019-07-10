4028d1237b555be8c266401e871fd16c55b15f9e
Soohaeng Yoo Willow <sy7willow@gmail.com>
2019-07-10 13:28:08 -0500

    Delete test.py

--- build_system/test.py
+++
@@ -1,9 +0,0 @@
-natom = 5
-nres  = 5
-x     = 1.0
-y     = 2.0
-z     = 3.0
-line = "HETATM " + "%4d"%natom + "  O   HOH  " + "%4d    "%nres +\
-       "%8.3f%8.3f%8.3f"%(x, y, z) + "  1.00  0.00           O"
-print line
-
